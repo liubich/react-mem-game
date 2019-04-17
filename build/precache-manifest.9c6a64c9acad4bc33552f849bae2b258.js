f7a7f70802ea846966bd15ebef496ee399b9d269
liubich <liubich.a@gmail.com>
2019-04-17 22:01:20 +0300

    Add production build

---
+++ build/precache-manifest.9c6a64c9acad4bc33552f849bae2b258.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "675628e081d46f15d6cd",
+    "url": "/liubich/react-mem-game/static/js/runtime~main.234af56c.js"
+  },
+  {
+    "revision": "c20447f08ad13e0b567a",
+    "url": "/liubich/react-mem-game/static/js/main.553e8a23.chunk.js"
+  },
+  {
+    "revision": "5c9ff16ec5bee154aee2",
+    "url": "/liubich/react-mem-game/static/js/2.5286435d.chunk.js"
+  },
+  {
+    "revision": "c20447f08ad13e0b567a",
+    "url": "/liubich/react-mem-game/static/css/main.aeb4dd9f.chunk.css"
+  },
+  {
+    "revision": "d3d3e929267f2730db397b972b384ae3",
+    "url": "/liubich/react-mem-game/index.html"
+  }
+];
