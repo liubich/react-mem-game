e91c243413b43dd7b4f4d211cad62984ddc6b6e7
liubich <liubich.a@gmail.com>
2019-04-19 20:00:25 +0300

    Fix test.js

--- src/ReactMemGame.test.js
+++ src/ReactMemGame.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ReactMemGame from './ReactMemGame';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<ReactMemGame />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
