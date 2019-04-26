baa34d687cad90163c528b46a2851c160866b806
liubich <liubich.a@gmail.com>
2019-04-26 10:49:52 +0300

    Inject difficulties array. Remove label

--- src/ChooseDifficulty/ChooseDifficulty.js
+++ src/ChooseDifficulty/ChooseDifficulty.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './ChooseDifficulty.css';
 
+const difficulties = [
+  {
+    title: "Easy",
+    value: 12
+  },
+  {
+    title: "Medium",
+    value: 20
+  },
+  {
+    title: "Hard",
+    value: 30
+  }
+]
+
 const ChooseDifficulty = (props) => {
     return (
       <div className = "modal" style = {{visibility: props.isDifficultyVisible ? "visible" : "hidden",}}>
         <div className = "modalContent">
           <p>Please, choose a level:</p>
-          <div className = "radioContainer">
+          {
+            difficulties.map((diffic, index) => (
+              <div className = "radioContainer">
+              <input type = "radio" name = "difficulty" id = {index} value = {diffic.value} onChange = {props.difficultyOnChange}> {diffic.title}
+              </input>
+          </div>
+            ))
+          }
+
+
+          {/* <div className = "radioContainer">
               <input type = "radio" name = "difficulty" id = "Easy" value = "12" onChange = {props.difficultyOnChange}>
               </input>
               <label htmlFor = "Easy">Easy</label>
@@ -20,7 +45,8 @@ const ChooseDifficulty = (props) => {
               <input type="radio" name="difficulty" id="Hard" value="30" onChange = {props.difficultyOnChange}>
               </input>
               <label htmlFor="Hard">Hard</label>
-          </div>
+          </div> */}
+          
             <input type="submit" value="Start" id="submit" disabled = {!props.isButtonEnabled} onClick = {props.buttonOnClick}>
             </input>
         </div>
