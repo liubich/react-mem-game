968509eb68f13fc69b715c51cebc54a1976c2679
liubich <liubich.a@gmail.com>
2019-07-28 23:28:40 +0300

    Fix connect and dispatch

--- src/RestartButton/RestartButton.js
+++ src/RestartButton/RestartButton.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import restartGame from "../redux/actions/gameActions";
+import { restartGame } from "../redux/actions/gameActions";
 import "./RestartButton.css";
 
 const RestartButton = props => {
   const restartButtonOnClick = () => {
-    props.dispatch(restartGame);
+    props.dispatch(restartGame());
   };
   return (
     <button className="restart-button" onClick={restartButtonOnClick}>
@@ -17,4 +17,5 @@ const RestartButton = props => {
 RestartButton.propTypes = {
   dispatch: PropTypes.func.isRequired,
 };
-export default connect(RestartButton);
+
+export default connect()(RestartButton);
