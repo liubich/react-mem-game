2d7bbb4c090436e67b082a10062fdf1dd9f4e8d8
liubich <liubich.a@gmail.com>
2019-08-17 20:49:41 +0300

    Prettifying

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactMemGame from './ReactMemGame';
-import configureStore from './redux/configureStore';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactMemGame from "./ReactMemGame";
+import configureStore from "./redux/configureStore";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 
 const store = configureStore({
-    fieldIsLocked: false,
-    timerIsActive: false,
-    secondsForTimer: 0,
-    isButtonEnabled: false,
-    isDifficultyVisible: true,
-    difficulties: [
-        {
-            title: "Easy",
-            value: 12
-        },
-        {
-            title: "Medium",
-            value: 20
-        },
-        {
-            title: "Hard",
-            value: 30
-        }
-    ],
+  fieldIsLocked: false,
+  timerIsActive: false,
+  secondsForTimer: 0,
+  isButtonEnabled: false,
+  isDifficultyVisible: true,
+  difficulties: [
+    {
+      title: "Easy",
+      value: 12,
+    },
+    {
+      title: "Medium",
+      value: 20,
+    },
+    {
+      title: "Hard",
+      value: 30,
+    },
+  ],
 });
 
 ReactDOM.render(
-    <Provider store={store} >
-        <ReactMemGame />
-    </Provider>,
-    document.getElementById('root'));
+  <Provider store={store}>
+    <ReactMemGame />
+  </Provider>,
+  document.getElementById("root"),
+);
 
 serviceWorker.register();
--- src/redux/reducers/gameReducer.js
+++ src/redux/reducers/gameReducer.js
@@ -1,61 +1,69 @@
 export default function gameReducer(state, action) {
-    let cardsProperties = [];
-    switch (action.type) {
-        case "DIFFICULTY_SUBMIT":
-            return {
-                ...state,
-                isDifficultyVisible: false,
-                numberOfCards: action.selectedDifficulty,
-                timeOfStart: new Date(),
-                timerIsActive: true,
-                cardsProperties: Array(action.selectedDifficulty).fill(0).map((_, index) => ({
-                    id: index,
-                    hiddenValue: Math.floor(index / 2),
-                    hidden: false,
-                    opened: false,
-                    positionOnField: Math.floor(Math.random() * 50)
-                }),
-                )
-            };
-        case "DIFFICULTY_PICK":
-            return {
-                ...state,
-                isButtonEnabled: true,
-                selectedDifficulty: action.selectedDifficulty,
-            }
-        case "HIDE_CARD":
-            cardsProperties = state.cardsProperties.map(value => Object.assign({}, value));
-            cardsProperties[action.id].hidden = true;
-            cardsProperties[action.id].opened = false;
-            return {
-                ...state,
-                cardsProperties
-            }
-        case "UNLOCK_FIELD":
-            return { ...state, fieldIsLocked: false };
-        case "LOCK_FIELD":
-            return { ...state, fieldIsLocked: true };
-        case "START_TIMER":
-            return { ...state, timerIsActive: true };
-        case "STOP_TIMER":
-            return { ...state, timerIsActive: false };
-        case "CLOSE_CARD":
-            cardsProperties = state.cardsProperties.map(value => Object.assign({}, value));
-            cardsProperties[action.id].opened = false;
-            return {
-                ...state,
-                cardsProperties
-            }
-        case "OPEN_CARD":
-            cardsProperties = state.cardsProperties.map(value => Object.assign({}, value));
-            cardsProperties[action.id].opened = true;
-            return {
-                ...state,
-                cardsProperties
-            }
-        case "SET_SECONDS":
-            return { ...state, secondsForTimer: action.seconds }
-        default:
-            return state;
-    }
+  let cardsProperties = [];
+  switch (action.type) {
+    case "DIFFICULTY_SUBMIT":
+      return {
+        ...state,
+        isDifficultyVisible: false,
+        numberOfCards: action.selectedDifficulty,
+        timeOfStart: new Date(),
+        timerIsActive: true,
+        cardsProperties: Array(action.selectedDifficulty)
+          .fill(0)
+          .map((_, index) => ({
+            id: index,
+            hiddenValue: Math.floor(index / 2),
+            hidden: false,
+            opened: false,
+            positionOnField: Math.floor(Math.random() * 50),
+          })),
+      };
+    case "DIFFICULTY_PICK":
+      return {
+        ...state,
+        isButtonEnabled: true,
+        selectedDifficulty: action.selectedDifficulty,
+      };
+    case "HIDE_CARD":
+      cardsProperties = state.cardsProperties.map(value =>
+        Object.assign({}, value),
+      );
+      cardsProperties[action.id].hidden = true;
+      cardsProperties[action.id].opened = false;
+      return {
+        ...state,
+        cardsProperties,
+      };
+    case "UNLOCK_FIELD":
+      return { ...state, fieldIsLocked: false };
+    case "LOCK_FIELD":
+      return { ...state, fieldIsLocked: true };
+    case "START_TIMER":
+      return { ...state, timerIsActive: true };
+    case "STOP_TIMER":
+      return { ...state, timerIsActive: false };
+    case "CLOSE_CARD":
+      cardsProperties = state.cardsProperties.map(value =>
+        Object.assign({}, value),
+      );
+      cardsProperties[action.id].opened = false;
+      return {
+        ...state,
+        cardsProperties,
+      };
+
+    case "OPEN_CARD":
+      cardsProperties = state.cardsProperties.map(value =>
+        Object.assign({}, value),
+      );
+      cardsProperties[action.id].opened = true;
+      return {
+        ...state,
+        cardsProperties,
+      };
+    case "SET_SECONDS":
+      return { ...state, secondsForTimer: action.seconds };
+    default:
+      return state;
+  }
 }
