19c1da1175072a41be1857a18613d7692b5cabb4
liubich <liubich.a@gmail.com>
2019-08-19 00:19:58 +0300

    Add images Import

---
+++ src/resources/images.js
@@ -0,0 +1,35 @@
+import backImg from "./react.svg";
+import img0 from "./0.svg";
+import img1 from "./1.svg";
+import img2 from "./2.svg";
+import img3 from "./3.svg";
+import img4 from "./4.svg";
+import img5 from "./5.svg";
+import img6 from "./6.svg";
+import img7 from "./7.svg";
+import img8 from "./8.svg";
+import img9 from "./9.svg";
+import img10 from "./10.svg";
+import img11 from "./11.svg";
+import img12 from "./12.svg";
+import img13 from "./13.svg";
+import img14 from "./14.svg";
+
+export const images = {
+  backImg: backImg,
+  "0": img0,
+  "1": img1,
+  "2": img2,
+  "3": img3,
+  "4": img4,
+  "5": img5,
+  "6": img6,
+  "7": img7,
+  "8": img8,
+  "9": img9,
+  "10": img10,
+  "11": img11,
+  "12": img12,
+  "13": img13,
+  "14": img14,
+};
