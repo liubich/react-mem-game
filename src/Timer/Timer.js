12e52d7335052f19617cf8df4141859791fb7690
liubich <liubich.a@gmail.com>
2019-11-03 22:05:21 +0200

    Props destructuring

--- src/Timer/Timer.js
+++ src/Timer/Timer.js
@@ -3,10 +3,10 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import "./Timer.css";
 
-const Timer = props => {
-  let minutes = Math.floor(props.secondsForTimer / 60);
+const Timer = ({ secondsForTimer }) => {
+  let minutes = Math.floor(secondsForTimer / 60);
   if (minutes < 10) minutes = `0${minutes}`;
-  let seconds = props.secondsForTimer % 60;
+  let seconds = secondsForTimer % 60;
   if (seconds < 10) seconds = `0${seconds}`;
   return <div id="timer">{`${minutes}:${seconds}`}</div>;
 };
